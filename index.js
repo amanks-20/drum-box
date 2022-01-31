b4ce28b576faa69adae846e00f120de1cb259c84
Aman <amanks20@iitk.ac.in>
2022-02-01 02:38:11 +0530

    press wasd and jkl

--- index.js
+++ index.js
@@ -14,43 +14,43 @@ function play(element){
 function handleSounds(position){
 
     switch (position) {
-        case 'first', '1':
+        case 'first', 'w':
 
             var firstAudio = new Audio('sounds/tom-1.mp3')
             firstAudio.play()
 
             break;
-        case 'second', '2':
+        case 'second', 'a':
 
             var secondAudio = new Audio('sounds/tom-2.mp3')
             secondAudio.play()
 
             break;
-        case 'third', '3':
+        case 'third', 's':
 
             var thirdAudio = new Audio('sounds/tom-3.mp3')
             thirdAudio.play()
 
             break;
-        case 'fourth', '4':
+        case 'fourth', 'd':
 
             var fourthAudio = new Audio('sounds/tom-4.mp3')
             fourthAudio.play()
 
             break;
-        case 'fiveth', '5':
+        case 'fiveth', 'j':
 
             var fivethAudio = new Audio('sounds/crash.mp3')
             fivethAudio.play()
 
             break;
-        case 'sixth', '6':
+        case 'sixth', 'k':
 
             var sixthAudio = new Audio('sounds/kick-bass.mp3')
             sixthAudio.play()
 
             break;
-        case 'seventh', '7':
+        case 'seventh', 'l':
 
             var seventhAudio = new Audio('sounds/snare.mp3')
             seventhAudio.play()
