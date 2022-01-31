183cbdbc54f36d458353338a5da456ee206e64db
Aman <amanks20@iitk.ac.in>
2022-02-01 02:34:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,43 +14,43 @@ function play(element){
 function handleSounds(position){
 
     switch (position) {
-        case 'first', 'w':
+        case 'first', '1':
 
             var firstAudio = new Audio('sounds/tom-1.mp3')
             firstAudio.play()
 
             break;
-        case 'second', 'a':
+        case 'second', '2':
 
             var secondAudio = new Audio('sounds/tom-2.mp3')
             secondAudio.play()
 
             break;
-        case 'third', 's':
+        case 'third', '3':
 
             var thirdAudio = new Audio('sounds/tom-3.mp3')
             thirdAudio.play()
 
             break;
-        case 'fourth', 'd':
+        case 'fourth', '4':
 
             var fourthAudio = new Audio('sounds/tom-4.mp3')
             fourthAudio.play()
 
             break;
-        case 'fiveth', 'j':
+        case 'fiveth', '5':
 
             var fivethAudio = new Audio('sounds/crash.mp3')
             fivethAudio.play()
 
             break;
-        case 'sixth', 'k':
+        case 'sixth', '6':
 
             var sixthAudio = new Audio('sounds/kick-bass.mp3')
             sixthAudio.play()
 
             break;
-        case 'seventh', 'l':
+        case 'seventh', '7':
 
             var seventhAudio = new Audio('sounds/snare.mp3')
             seventhAudio.play()
